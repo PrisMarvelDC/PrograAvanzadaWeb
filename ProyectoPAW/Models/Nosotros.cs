2dc7affb21cb6a32b4d0852c88be2b77ed6f0eb9
Juan Diego Marin Herrera <94065036+JuanM-25@users.noreply.github.com>
2024-04-20 20:00:36 -0600

    Commit JSON
    Se pulio las vistas de Cursos, se añadio JSON de nosotros

---
+++ ProyectoPAW/Models/Nosotros.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoPAW.Models
+{
+    public class Nosotros
+    {
+        public string NombreComercial { get; set; }
+        public string Telefono { get; set; }
+        public string CorreoVentas { get; set; }
+        public string CorreoInfo { get; set; }
+    }
+}
